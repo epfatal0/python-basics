d740fcba5c31f72f4f78b67774520fb48f1b3f99
epfatal0 <ericafatal@gmail.com>
2025-07-16 17:54:29 -0400

    updates

---
+++ String Manipulation in Python.py
@@ -0,0 +1,56 @@
+to_be_changed = "John Glenn|Neil Armstrong|Sally Ride|Douglass Wheelock|Mae Jemison"
+changed_values = to_be_changed.split("|")
+#print(changed_values)
+
+lyrics = """
+Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"
+"""
+lyrics_split = lyrics.split()
+#print(lyrics_split)
+
+lyrics = """
+Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"
+"""
+changed_values = lyrics
+#print(changed_values)
+
+long_village_name = "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"
+string_length = long_village_name
+#print(len(string_length))
+
+my_path = '   /c/Users/instructor/Downloads/Submit Relating the Nonrelational
+Assessment Download May 10, 2021 917 AM      '
+my_folders = my_path.strip()
+print(my_folders)
+
+composers="Beethoven,Ludwig von;Liszt,Franz;Mozart,Wolfgang;Copland,Aaron"
+# Separate the composers
+composers_split = composers
+# Get the third composer
+third_composer = composers_split[2]
+# Find the comma in the name
+comma_position = third_composer
+# Use the slicing notation to get the last name
+last_name = third_composer[]
+# Use the slicing notation to get the first name
+first_name = third_composer[]
+# Join the names to get the 3rd composer's name in "first last" format
+third_composer_name
+# Print the composer's name
+print(third_composer_name)
---
+++ Variables in Python excercise.py
@@ -0,0 +1,49 @@
+onomatopeia = 'pop'
+print(type(onomatopeia))
+
+lyrics = """Katie Casey was baseball mad,
+Had the fever and had it bad.
+Just to root for the home town crew,
+Ev'ry sou
+Katie blew.
+On a Saturday her young beau
+Called to see if she'd like to go
+To see a show, but Miss Kate said "No,
+I'll tell you what you can do:"
+"""
+#print(lyrics)
+
+gpa = 3.75
+#print(gpa)
+#print(type(gpa))
+
+empty_var = None
+#print(empty_var)
+#print(type(empty_var))
+
+result = 828**282
+#print(result)
+
+remainder= (52-52) + (64/8) + (42%8)
+print(remainder)
+
+true = True
+isTrue = true is not False
+#print(isTrue)
+
+weird_word = 'Weird'
+check = weird_word=='weird'
+print(check)
+
+#false = 
+#the variable is not defined
+
+value = 35/7
+value2 = 15**3
+value3 = value2 % value
+value4 = value2 / value
+print(value)
+print(value2)
+print(value3)
+print(value4)
+
